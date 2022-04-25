f29ec0d4f2e3992b77d77e46795f44c0ab999c4a
Breno Vergilio <bjvergilio47@gmail.com>
2022-04-25 15:25:13 -0400

    changing titles dinamicaly

--- src/app/views/feriado-crud/feriado-crud.component.ts
+++ src/app/views/feriado-crud/feriado-crud.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HeaderService } from 'src/app/components/template/header/header.service';
 
 @Component({
   selector: 'app-feriado-crud',
@@ -8,7 +9,13 @@ import { Router } from '@angular/router';
 })
 export class FeriadoCrudComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private headerService: HeaderService) { 
+    headerService.headerData = {
+      title: 'Feriados',
+      icon: 'edit_calendar',
+      routeUrl: '/feriados'
+    }
+   }
 
   ngOnInit(): void {
   }
--- src/app/views/medidor-crud/medidor-crud.component.ts
+++ src/app/views/medidor-crud/medidor-crud.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HeaderService } from 'src/app/components/template/header/header.service';
 
 @Component({
   selector: 'app-medidor-crud',
@@ -8,7 +9,13 @@ import { Router } from '@angular/router';
 })
 export class MedidorCrudComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private headerService: HeaderService) { 
+    headerService.headerData = {
+      title: 'Medidores',
+      icon: 'flash_on',
+      routeUrl: '/medidores'
+    }
+   }
 
   ngOnInit(): void {
   }
