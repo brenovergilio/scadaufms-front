3e5a7edf34a937634b9819507eca7482d2df81b7
breno <bjvergilio47@gmail.com>
2023-04-14 20:26:50 -0400

    feat: final commit

--- src/app/components/tabs/medicoes-table/medicoes-table.component.ts
+++ src/app/components/tabs/medicoes-table/medicoes-table.component.ts
@@ -9,7 +9,7 @@ import * as XLSX from 'xlsx';
 @Component({
   selector: 'app-medicoes-table',
   templateUrl: './medicoes-table.component.html',
-  styleUrls: ['./medicoes-table.component.css']
+  styleUrls: [ './medicoes-table.component.css' ]
 })
 export class MedicoesTableComponent implements OnChanges, AfterViewInit {
   @Input() inputs: Medicao[]
@@ -18,15 +18,15 @@ export class MedicoesTableComponent implements OnChanges, AfterViewInit {
   dataSource: MedicoesTableDataSource;
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['Data/Hora', 'Tensão Fase A (V)','Tensão Fase B (V)', 'Tensão Fase C (V)', 'Corrente Fase A (A)', 'Corrente Fase B (A)', 'Corrente Fase C (A)', 'Potência Ativa Total (kW)', 'Potência Reativa Total (kVAr)', 'Potência Aparente Total (VA)', 'Fator De Potência'];
+  displayedColumns = [ 'Data/Hora', 'Tensão Fase A (V)', 'Tensão Fase B (V)', 'Tensão Fase C (V)', 'Corrente Fase A (A)', 'Corrente Fase B (A)', 'Corrente Fase C (A)', 'Potência Ativa Total (kW)', 'Potência Reativa Total (kVAr)', 'Potência Aparente Total (kVA)', 'Fator De Potência' ];
 
   constructor() {
     this.dataSource = new MedicoesTableDataSource();
   }
 
   ngOnChanges(): void {
-    if(this.inputs) this.dataSource.data = this.inputs;
-    if(this.paginator) this.paginator._changePageSize(this.paginator.pageSize);
+    if (this.inputs) this.dataSource.data = this.inputs;
+    if (this.paginator) this.paginator._changePageSize(this.paginator.pageSize);
   }
 
   ngAfterViewInit(): void {
@@ -36,12 +36,12 @@ export class MedicoesTableComponent implements OnChanges, AfterViewInit {
 
   downloadMeasurementCSV() {
     const arrayOfMeasurements = this.convertInputToArray();
-    const nameOfFile = `medicoes-${arrayOfMeasurements[1][0].toString().replace(' ', '-')}-${arrayOfMeasurements[arrayOfMeasurements.length-1][0].toString().replace(' ', '-')}.csv`;
+    const nameOfFile = `medicoes-${arrayOfMeasurements[ 1 ][ 0 ].toString().replace(' ', '-')}-${arrayOfMeasurements[ arrayOfMeasurements.length - 1 ][ 0 ].toString().replace(' ', '-')}.csv`;
     const csv = arrayOfMeasurements.map((row) => row.join(','));
     const csvFile = csv.join('\r\n');
-  
+
     const a = document.createElement('a');
-    const blob = new Blob([csvFile], { type: 'text/csv' });
+    const blob = new Blob([ csvFile ], { type: 'text/csv' });
     const url = window.URL.createObjectURL(blob);
     a.href = url;
     a.download = nameOfFile;
@@ -52,9 +52,9 @@ export class MedicoesTableComponent implements OnChanges, AfterViewInit {
 
   downloadMeasurementXLSX() {
     const arrayOfMeasurements = this.convertInputToArray();
-    const nameOfFile = `medicoes-${arrayOfMeasurements[1][0].toString().replace(' ', '-')}-${arrayOfMeasurements[arrayOfMeasurements.length-1][0].toString().replace(' ', '-')}.xlsx`;
-    const ws: XLSX.WorkSheet =XLSX.utils.aoa_to_sheet(arrayOfMeasurements);
- 
+    const nameOfFile = `medicoes-${arrayOfMeasurements[ 1 ][ 0 ].toString().replace(' ', '-')}-${arrayOfMeasurements[ arrayOfMeasurements.length - 1 ][ 0 ].toString().replace(' ', '-')}.xlsx`;
+    const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(arrayOfMeasurements);
+
     const wb: XLSX.WorkBook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(wb, ws, 'Medições');
     XLSX.writeFile(wb, nameOfFile);
@@ -63,12 +63,12 @@ export class MedicoesTableComponent implements OnChanges, AfterViewInit {
   convertInputToArray() {
     const arrayOfMeasurements = [];
     arrayOfMeasurements.push(this.displayedColumns);
-    for(const input of this.inputs) {
+    for (const input of this.inputs) {
       const currentRow = [];
       currentRow.push(input.timestamp)
-      for(const value of Object.entries(input.values))
-        currentRow.push(value[1])
-      arrayOfMeasurements.push(currentRow)  
+      for (const value of Object.entries(input.values))
+        currentRow.push(value[ 1 ])
+      arrayOfMeasurements.push(currentRow)
     }
     return arrayOfMeasurements;
   }
